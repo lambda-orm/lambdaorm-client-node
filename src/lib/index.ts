1ad40ad6d316d56e72dfe699559201c5ba056197
FlavioLionelRita <flaviolrital@hotmail.com>
2023-11-19 03:09:47 +0100

    feat: rename OrmClient for Orm
    #0

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,4 @@ import { Orm } from './infrastructure/adapters/orm'
 export * from './application'
 export * from './infrastructure'
 export * from './domain'
-export { Orm } from './infrastructure/adapters/orm'
 export const orm = Orm.instance
